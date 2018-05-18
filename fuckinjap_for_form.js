fbcc0a81c56abaae8185e95b91b23429ba8783a7
kenta.hatano <kenta.hatano@s-cubism.jp>
2018-05-18 20:44:51 +0900

    プリセットを追加

--- fuckinjap_for_form.js
+++ fuckinjap_for_form.js
@@ -31,3 +31,35 @@ function runBrainfuck() {
 	}
 }
 
+function changePreset(which) {
+	if (which.value == "fuckinjap_1") {
+		document.getElementById('chars_plus').value = "ゑあけうつわちいねかりろびざぶぽぎごょば";
+		document.getElementById('chars_minus').value = "ひさふゐもきこのなよぬはぁげぅづゎぢぃが"; 
+		document.getElementById('chars_lt').value = "せゆえおらたるにずぺでぐぷぼ";
+		document.getElementById('chars_gt').value = "すめてくむれをほぜゅぇぉだ";
+		document.getElementById('chars_while').value = "そへぴゃ";
+		document.getElementById('chars_end').value = "みやぞべ";
+		document.getElementById('chars_dot').value = "しまとっ。";
+		document.getElementById('chars_comma').value = "んじぱど、";
+	} else if (which.value == "fuckinjap_1_1") {
+		document.getElementById('chars_plus').value = "あけうつわちいねかりろあけうつわちいねかりろびざぶぽぎごょば";
+		document.getElementById('chars_minus').value = "ひさふもきこのなよぬはひさふもきこのなよぬはぁげぅづゎぢぃが"; 
+		document.getElementById('chars_lt').value = "せゆえおらたるにせゆえおらたるにずぺでぐぷぼ";
+		document.getElementById('chars_gt').value = "すめてくむれをほすめてくむれほぜゅぇぉだ";
+		document.getElementById('chars_while').value = "そへそへぴゃ";
+		document.getElementById('chars_end').value = "みやみやぞべ";
+		document.getElementById('chars_dot').value = "しまとしまとっ。。";
+		document.getElementById('chars_comma').value = "んんじぱど、、";
+	} else if (which.value == "monamona") {
+		document.getElementById('chars_plus').value = "M";
+		document.getElementById('chars_minus').value = "O"; 
+		document.getElementById('chars_lt').value = "N";
+		document.getElementById('chars_gt').value = "A";
+		document.getElementById('chars_while').value = "m";
+		document.getElementById('chars_end').value = "o";
+		document.getElementById('chars_dot').value = "n";
+		document.getElementById('chars_comma').value = "a";
+	}
+}
+
+
